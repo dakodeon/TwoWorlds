75574c2b3f39c00f19e96068b7a64a0af881d6de
dakodeon <dakodeon@hotmail.com>
2015-10-28 18:02:25 +0200

    Updated getIP.py

--- remoteServer/getIP.py
+++ remoteServer/getIP.py
@@ -12,27 +12,25 @@ import time
 client = OSC.OSCClient()
 client.connect( ('127.0.0.1', 57120) ) # Connecting to local SuperCollider
 
-ip = myIP = ipgetter.myip()
+myIP = ipgetter.myip()
 
 timeInt = 3
 
 print "My IP is " + myIP
 
 while True:
-    start = time.time()
-    ip = ipgetter.myip()
-    if ip != myIP:
+    # start = time.time()
+    # ip = ipgetter.myip()
+    # if ip != myIP:
         # check again
-        if ip != myIP:
-            print "Check again..."
-            myIP = ip
-            print "The IP changed to " + myIP
-            msg = OSC.OSCMessage()
-            msg.setAddress("/ip")
-            msg.append(myIP)
-            client.send(msg)
-    
+        # if ip != myIP:
+            # print "Check again..."
+    myIP = ipgetter.myip()
+    print "The public IP is " + myIP
+    msg = OSC.OSCMessage()
+    msg.setAddress("/ip")
+    msg.append(myIP)
+    client.send(msg)
     # Wait some time
-    print "My IP: " + myIP + " || time: " + str(time.time() - start)
+    # print "My IP: " + myIP + " || time: " + str(time.time() - start)
     time.sleep(timeInt)
-
